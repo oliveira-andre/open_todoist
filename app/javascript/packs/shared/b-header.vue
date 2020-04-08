b8c7c09f4af5dbfd28179e64884a25eabf8dc443
Andre Oliveira <desmonplays@gmail.com>
2020-04-07 20:09:42 -0400

    (new feature) animating input on header

---
+++ app/javascript/packs/shared/b-header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="bHeader">
+    <header>
+      <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          <div class="left-side">
+            <i class="fas fa-bars"></i>
+            <input placholder="Fast Search" class="input input-search">
+          </div>
+        </div>
+      </nav>
+    </header>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+      }
+    }
+  }
+</script>
+
