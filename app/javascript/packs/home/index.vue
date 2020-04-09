f9101d15cf051adad4769676af6a291e52fcd3fb
Andre Oliveira <desmonplays@gmail.com>
2020-04-08 20:49:32 -0400

    (new feature) if don't have tasks show message

--- app/javascript/packs/home/index.vue
+++ app/javascript/packs/home/index.vue
@@ -1,22 +1,32 @@
 <template>
   <div id="home">
-    <p>{{ message }}</p>
+    <div class="container">
+      <div class="columns is-multiline">
+        <div class="column is-12 is-flex">
+          <h1 class="title is-size-4">
+            Caixa de entrada
+          </h1>
+          <div class="right-side">
+            <i class="far fa-comment-alt is-size-5"></i>
+            <i class="fas fa-ellipsis-h is-size-5"></i>
+          </div>
+        </div>
+        <div class="column is-12 has-text-centered">
+          <div v-if="has_tasks">
+            teste
+          </div>
+          <div v-else class="center">
+            <p>Parece que você não tem nada agendado</p>
+            <button class="button is-danger is-small">Adicionar uma tarefa</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
-    }
-  }
+  props: ["tasks", "has_tasks"]
 }
 </script>
-
-<style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
-</style>
