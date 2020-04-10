63c067ac6b66635535428387978e2cc2abf51873
Andre Oliveira <desmonplays@gmail.com>
2020-04-09 20:18:05 -0400

    (new feature) point and commas on requires

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,12 @@
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
 import TurbolinksAdapter from 'vue-turbolinks';
 import Vue from 'vue/dist/vue.esm';
 import Home from './home/index';
-import bHeader from './shared/b-header'
+import bHeader from './shared/b-header';
 
 Vue.use(TurbolinksAdapter);
 Vue.component('b-header', bHeader);
