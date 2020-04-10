e0707e135227210d5ecb34a78d2d34e3d2775283
Andre Oliveira <desmonplays@gmail.com>
2020-04-10 18:59:39 -0400

    (new feature) importing the act_as_authenticable and validating the score_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  acts_as_token_authenticatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  before_validation :vinculate_score
 
   enum status: { active: 0, deactive: 1 }
 
@@ -13,4 +16,10 @@ class User < ApplicationRecord
   has_many :tasks
 
   validates :email, :password, :status, :score_id, presence: true
+
+  private
+
+  def vinculate_score
+    self.score_id = Score.create.id
+  end
 end
