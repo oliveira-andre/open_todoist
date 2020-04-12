70137c3a4f6977922ddc8b4efa45acbf9b3ef423
Andre Oliveira <desmonplays@gmail.com>
2020-04-11 20:36:46 -0400

    (new feature) skiping verify authenticity token loading token, current user and user_signed_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
+
+  def user_signed_in?
+    current_user.present?
+  end
+
+  def current_user
+    User.find_by(authentication_token: token)
+  end
+
+  private
+
+  def token
+    cookies['Authorization']&.delete('Bearer ')
+  end
 end
