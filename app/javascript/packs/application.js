1b044dfe9162b99f1c4b94df2eb3b085184cda90
Andre Oliveira <desmonplays@gmail.com>
2020-04-12 16:12:01 -0400

    (new feature) importing vue resources and component bsidebar and adding the csrf token on all html

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,16 +3,21 @@ require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
 
-import TurbolinksAdapter from 'vue-turbolinks';
 import Vue from 'vue/dist/vue.esm';
+import TurbolinksAdapter from 'vue-turbolinks';
+import VueResource from 'vue-resource';
 import Home from './home/index';
 import bHeader from './shared/b-header';
+import bSidebar from './shared/b-sidebar';
 
+Vue.use(VueResource);
 Vue.use(TurbolinksAdapter);
 Vue.component('b-header', bHeader);
+Vue.component('b-sidebar', bSidebar);
 Vue.component('home', Home);
 
 document.addEventListener("turbolinks:load", function() {
+  Vue.http.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
   const app = new Vue({
     el: '#app',
   });
