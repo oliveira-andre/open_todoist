ad377e0e56936b6c994d6809983c6e299d42749f
Andre Oliveira <desmonplays@gmail.com>
2020-04-11 20:37:19 -0400

    (new feature) fixing the props of vue and html

--- app/javascript/packs/shared/b-header.vue
+++ app/javascript/packs/shared/b-header.vue
@@ -7,38 +7,36 @@
             <i class="fas fa-bars"></i>
             <input placeholder="Fast Search" class="input input-search">
           </div>
-          <div class="right-side">
-            <div v-if="is_signed_in">
-              <div class="item is-size-5">
-                <a href="#">
-                  <i class="fas fa-cog"></i>
-                </a>
-              </div>
-              <div class="item is-size-5">
-                <a href="#">
-                  <i class="fas fa-bell"></i>
-                </a>
-              </div>
-              <div class="item is-size-5">
-                <a href="">
-                  <i class="fas fa-chart-line"></i>
-                  <span class="is-size-6">
-                    0/5
-                  </span>
-                </a>
-              </div>
-              <div class="item is-size-5">
-                <a href="#">
-                  <i class="fas fa-plus"></i>
-                </a>
-              </div>
+          <div class="right-side" v-if="is_signed_in">
+            <div class="item is-size-5">
+              <a href="#">
+                <i class="fas fa-cog"></i>
+              </a>
             </div>
-            <div v-else>
-              <div class="item">
-                <button class="button is-danger is-small" data-target="login-modal" v-on:click="openLoginModal">
-                  Login
-                </button>
-              </div>
+            <div class="item is-size-5">
+              <a href="#">
+                <i class="fas fa-bell"></i>
+              </a>
+            </div>
+            <div class="item is-size-5">
+              <a href="">
+                <i class="fas fa-chart-line"></i>
+                <span class="is-size-6">
+                  0/5
+                </span>
+              </a>
+            </div>
+            <div class="item is-size-5">
+              <a href="#">
+                <i class="fas fa-plus"></i>
+              </a>
+            </div>
+          </div>
+          <div class="right-side" v-else>
+            <div class="item">
+              <button class="button is-danger is-small" data-target="login-modal" v-on:click="openLoginModal">
+                Login
+              </button>
             </div>
           </div>
         </div>
@@ -46,25 +44,27 @@
       <div class="modal" id="login-modal">
         <div class="modal-background" v-on:click="closeLoginModal"></div>
         <div class="modal-content">
-          <div class="field">
-            <label class="label" for="email">
-              Email
-            </label>
-            <div class="control">
-              <input class="input">
+          <form v-on:submit.prevent="onSubmit">
+            <div class="field">
+              <label class="label" for="email">
+                Email
+              </label>
+              <div class="control">
+                <input class="input" type="email" id="email" v-model="email">
+              </div>
             </div>
-          </div>
-          <div class="field">
-            <label class="label" for="password">
-              Password
-            </label>
-            <div class="control">
-              <input class="input">
+            <div class="field">
+              <label class="label" for="password">
+                Password
+              </label>
+              <div class="control">
+                <input class="input" type="password" id="password" v-model="password">
+              </div>
             </div>
-          </div>
-          <button class="button is-danger is-fullwidth">
-            Login
-          </button>
+            <button class="button is-danger is-fullwidth" type="submit">
+              Login
+            </button>
+          </form>
         </div>
       </div>
     </header>
@@ -72,20 +72,41 @@
 </template>
 
 <script>
-  export default {
-    data: function () {
-      return {
-        props: ["current_user", "is_signed_in"]
-      }
+  import axios from 'axios';
+  import toastr from 'toastr/toastr';
+
+export default {
+  props: ["current_user", "is_signed_in"],
+  data: function () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    openLoginModal: function(event) {
+      const modal = document.getElementById(event.target.dataset.target);
+      modal.classList.add('is-active');
     },
-    methods: {
-      openLoginModal: function(event) {
-          const modal = document.getElementById(event.target.dataset.target);
-          modal.classList.add('is-active');
+    closeLoginModal: function(event) {
+      event.target.parentNode.classList.remove('is-active');
+    },
+    onSubmit: function() {
+        axios.post('http://localhost:3000/users/sign_in', {
+          users: { 
+            email: email.value,
+            password: password.value 
+          } 
+        }).then(response => { 
+          if(response.status == 202) {
+            Turbolinks.visit('/');
+            toastr.success(response.data.message);
+          }
+        })
+          .catch ( e => { 
+            toastr.error('Email or Password are wrong');
+          });
       },
-      closeLoginModal: function(event) {
-          event.target.parentNode.classList.remove('is-active');
-      }
     }
   }
 </script>
