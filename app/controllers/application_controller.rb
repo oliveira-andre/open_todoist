46527362bc3e928b1279ea741423796e05aa925f
Andre Oliveira <desmonplays@gmail.com>
2020-04-12 16:11:28 -0400

    (new feature) removing the skip of verifying authenticy token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  skip_before_action :verify_authenticity_token
-
   def user_signed_in?
     current_user.present?
   end
