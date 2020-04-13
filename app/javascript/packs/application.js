a3d36d30f9562c9a726a5b2eb36b0b9504b363c5
Andre Oliveira <desmonplays@gmail.com>
2020-04-12 22:35:03 -0400

    (new feature) config vue router

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,20 +6,19 @@ require("channels");
 import Vue from 'vue/dist/vue.esm';
 import TurbolinksAdapter from 'vue-turbolinks';
 import VueResource from 'vue-resource';
-import Home from './home/index';
-import bHeader from './shared/b-header';
-import bSidebar from './shared/b-sidebar';
+
+import router from './routes.js';
+import App from './App.vue';
 
 Vue.use(VueResource);
 Vue.use(TurbolinksAdapter);
-Vue.component('b-header', bHeader);
-Vue.component('b-sidebar', bSidebar);
-Vue.component('home', Home);
 
 document.addEventListener("turbolinks:load", function() {
   Vue.http.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
   const app = new Vue({
     el: '#app',
+    router,
+    render: h => h(App)
   });
 });
 
