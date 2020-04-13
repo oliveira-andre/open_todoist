2c296d7bfd52858080cdc83568c34637d5aa8f8f
Andre Oliveira <desmonplays@gmail.com>
2020-04-12 22:31:13 -0400

    (new feature) routing the react and the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  namespace :api do
+    namespace :v1 do
+      resources :sessions, only: :create
+      resources :users, only: %i[show], param: :token
+    end
+  end
+
   root to: 'home#index'
+  get '/*path', to: 'home#index'
 end
