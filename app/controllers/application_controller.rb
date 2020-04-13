d465782a5b2f3826bf3d2aba9bffc4157e172020
Andre Oliveira <desmonplays@gmail.com>
2020-04-12 22:32:55 -0400

    (new feature) importing the response concern and removing the application methods coz not will be usefull

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  def user_signed_in?
-    current_user.present?
-  end
-
-  def current_user
-    User.find_by(authentication_token: token)
-  end
-
-  private
-
-  def token
-    cookies['Authorization']&.delete('Bearer ')
-  end
+  include Response
 end
