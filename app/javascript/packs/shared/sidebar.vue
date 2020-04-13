725e4c37719e0b71b49663ca4f3bc19da8a10240
Andre Oliveira <desmonplays@gmail.com>
2020-04-13 00:03:59 -0400

    (new feature) switching between tabs using the vue router link

--- app/javascript/packs/shared/sidebar.vue
+++ app/javascript/packs/shared/sidebar.vue
@@ -2,23 +2,23 @@
   <div id="bSidebar">
     <ul class="main_filter is-flex">
       <div class="columns is-multiline">
-        <li v-bind:class="{ current: isHome }">
-          <a href="/">
+        <li v-on:click="switchCurrentTab" class="current">
+          <router-link v-on:click="switchCurrentTab" to="/">
            <i class="fas fa-inbox has-text-primary is-size-6"></i> 
             Caixa de entrada
-          </a>
+          </router-link>
         </li>
-        <li v-bind:class="{ current: isToday }">
-          <a v-on:click="switchCurrentTab" data-time="today">
+        <li v-on:click="switchCurrentTab">
+          <router-link to="/today">
             <i class="far fa-calendar has-text-info is-size-6"></i>
             Hoje
-          </a>
+          </router-link>
         </li>
-        <li v-bind:class="{ current: isWeek }">
-          <a v-on:click="switchCurrentTab" data-time="week">
+        <li v-on:click="switchCurrentTab">
+          <router-link to="/week">
             <i class="far fa-calendar-alt has-text-warning is-size-6"></i>
             Próximos 7 dias
-          </a>
+          </router-link>
         </li>
       </div>
     </ul>
@@ -29,29 +29,12 @@
     export default {
       data: function() {
         return {
-          isHome: false,
-          isToday: false,
-          isWeek: false,
         }
-      },
-      created: function() {
-        let urlParams = new URLSearchParams(window.location.search);
-        let time = urlParams.get('time');
-
-        switch(time) {
-          case 'today':
-            this.isToday = true
-            break;
-          case 'week':
-            this.isWeek = true
-            break;
-          default:
-            this.isHome = true
-        };
       },
       methods: {
         switchCurrentTab: function(event) {
-          Turbolinks.visit(`/?time=${event.target.dataset.time}`);
+          document.getElementsByClassName('current')[0].classList.remove('current');
+          event.target.parentNode.classList.add('current');
         },
       },
     }
