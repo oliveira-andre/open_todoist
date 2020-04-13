e81758f65ae17a591a206ea26a09ad4b8303fb98
Andre Oliveira <desmonplays@gmail.com>
2020-04-12 22:33:32 -0400

    (new feature) response utils methods

---
+++ app/controllers/concerns/response.rb
@@ -0,0 +1,19 @@
+module Response
+   def success_response(data:, model:, includes: '', status: :ok)
+    return unless data || model
+
+    if data.respond_to?('each')
+      render json: data, each_serializer: "Api::V1::#{model}Serializer".constantize,
+             includes: includes.split(','), status: status
+    else
+      render json: data, serializer: "Api::V1::#{model}Serializer".constantize,
+             includes: includes.split(','), status: status
+    end
+  end
+
+  def error_response(data:)
+    return unless data
+
+    render json: data.errors, status: :unprocessable_entity
+  end
+end
