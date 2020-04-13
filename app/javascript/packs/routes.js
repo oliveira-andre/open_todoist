98d4fca16690e9dd6a9ead14ac170e92e7490cc6
Andre Oliveira <desmonplays@gmail.com>
2020-04-13 00:04:15 -0400

    (new feature) initializing the week and today route

--- app/javascript/packs/routes.js
+++ app/javascript/packs/routes.js
@@ -3,10 +3,14 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
 import HomeIndex from './home/index';
+import TodayIndex from './today/index';
+import WeekIndex from './week/index';
 
 export default new VueRouter({
   mode: 'history',
   routes: [
-    { path: '/', component: HomeIndex }
+    { path: '/', component: HomeIndex },
+    { path: '/today', component: TodayIndex },
+    { path: '/week', component: WeekIndex },
   ],
 });
---
+++ app/javascript/packs/week/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="today"></div>
+</template>
+
+<script>
+  export default {
+    data: function() {
+      return {}
+    }
+  }  
+</script>
