c40d9225b877ef7664c5d6ca9d13a274d9031027
Andre Oliveira <desmonplays@gmail.com>
2020-04-13 00:03:00 -0400

    (new feature) fixing the router

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,18 +6,20 @@ require("channels");
 import Vue from 'vue/dist/vue.esm';
 import TurbolinksAdapter from 'vue-turbolinks';
 import VueResource from 'vue-resource';
+import VueRouter from 'vue-router'
 
 import router from './routes.js';
 import App from './App.vue';
 
 Vue.use(VueResource);
 Vue.use(TurbolinksAdapter);
+Vue.use(VueRouter);
 
 document.addEventListener("turbolinks:load", function() {
   Vue.http.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
   const app = new Vue({
     el: '#app',
-    router,
+    router: router,
     render: h => h(App)
   });
 });
