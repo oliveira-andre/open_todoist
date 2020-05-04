d27317a72ff761200e175c2897590c2ef0c24c53
Andre Oliveira <desmonplays@gmail.com>
2020-05-04 18:30:46 -0400

    (new feature) loading all projects and your tasks

--- app/javascript/packs/home/index.vue
+++ app/javascript/packs/home/index.vue
@@ -4,20 +4,29 @@
       <div class="columns is-multiline">
         <div class="column is-12 is-flex">
           <p class="title is-size-4">
-            Caixa de entrada
+            inbox
           </p>
           <div class="right-side">
             <i class="far fa-comment-alt is-size-5"></i>
             <i class="fas fa-ellipsis-h is-size-5"></i>
           </div>
         </div>
-        <div class="column is-12 has-text-centered">
-          <div v-if="has_tasks">
-            <p>teste</p>
+        <div class="column is-12">
+          <div v-if="has_projects">
+            <ul class="projects">
+              <template v-for="project in projects">
+                <li>{{ project.title }}</li>
+                <ul class="tasks">
+                  <template v-for="task in project.tasks">
+                    <li>{{ task.title }}</li>
+                  </template>
+                </ul>
+              </template>
+            </ul>
           </div>
-          <div v-else class="center">
-            <p>Parece que você não tem nada agendado</p>
-            <button class="button is-danger is-small">Adicionar uma tarefa</button>
+          <div v-else class="has-text-centered center">
+            <p>Appearly you don't ave any tasks</p>
+            <button class="button is-danger is-small">New Task</button>
           </div>
         </div>
       </div>
@@ -29,20 +38,20 @@
 export default {
   data: function() {
     return {
-      has_tasks: false,
-      tasks: [],
+      has_projects: false,
+      projects: [],
       token: '',
     }
   },
   created: function() {
     this.token = localStorage.getItem('token');
     if(this.token !== '') {
-      this.$http.get('/api/v1/tasks', {
+      this.$http.get('/api/v1/projects', {
         headers: { token: this.token }
       }).then(response => {
         if(response.status == 200) {
-          this.tasks = response.body;
-          this.has_tasks = this.tasks.length !== 0;
+          this.projects = response.body;
+          this.has_projects = this.projects.length !== 0;
         }
       });
     }
