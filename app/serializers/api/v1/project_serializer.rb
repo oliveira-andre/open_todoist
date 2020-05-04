d80f79dd43b24e00f98f521fd2c4dae01df7b066
Andre Oliveira <desmonplays@gmail.com>
2020-05-04 18:29:23 -0400

    (new feature) now the project render an array of tasks

--- app/serializers/api/v1/project_serializer.rb
+++ app/serializers/api/v1/project_serializer.rb
@@ -4,6 +4,8 @@ module Api
   module V1
     class ProjectSerializer < ActiveModel::Serializer
       attributes :id, :title, :schedule_date
+
+      has_many :tasks, each_serializer: TaskSerializer
     end
   end
 end
--- app/serializers/api/v1/task_serializer.rb
+++ app/serializers/api/v1/task_serializer.rb
@@ -4,8 +4,6 @@ module Api
   module V1
     class TaskSerializer < ActiveModel::Serializer
       attributes :id, :title, :description, :status, :schedule_date
-
-      has_one :project
     end
   end
 end
