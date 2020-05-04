53a89505d130216108228fc6219b95c7acf828c3
Andre Oliveira <desmonplays@gmail.com>
2020-05-04 18:29:38 -0400

    (new feature) routing project

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :sessions, only: :create
       resources :users, only: %i[show], param: :token
-      resources :tasks, only: :index
+      resources :projects, only: :index
     end
   end
 
