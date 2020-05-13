8e2dc253f4a3889a2773bedb3193390c774da87f
Andre Oliveira <desmonplays@gmail.com>
2020-05-12 21:52:07 -0400

    (new feature) creating project and returning it

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -3,16 +3,31 @@
 module Api
   module V1
     class ProjectsController < ApplicationController
-      before_action :load_user, :load_projects
+      before_action :load_user
+      before_action :load_projects, only: :index
 
       def index
         success_response(data: @projects, model: 'Project')
       end
 
+      def create
+        @project = Project.create!(projects_params)
+
+        if @project
+          success_response(data: @project, model: 'Project', status: :created)
+        else
+          error_response
+        end
+      end
+
       private
 
+      def projects_params
+        params.require(:projects).permit(:title).merge(user: @user)
+      end
+
       def load_projects
-        @projects = Project.joins(:tasks).where(tasks: { user_id: @user.id })
+        @projects = @user.projects
       end
     end
   end
