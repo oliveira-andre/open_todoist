a321001c2ad24d9480d00a34a0e1ba409af06b4a
Andre Oliveira <desmonplays@gmail.com>
2020-05-17 19:05:05 -0400

    (new feature) resourcing project_tasks only create

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :sessions, only: :create
       resources :users, only: %i[show], param: :token
-      resources :projects, only: %i[index create]
+      resources :projects, only: %i[index create] do
+        resources :tasks, only: :create
+      end
     end
   end
 
