80e0397fc40c8b396eeed224b4e9a0983616739a
Andre Oliveira <desmonplays@gmail.com>
2020-05-18 20:16:42 -0400

    (new feature) archiving tasks

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -4,6 +4,7 @@ module Api
   module V1
     class TasksController < ApplicationController
       before_action :load_user, :load_project
+      before_action :load_task, only: :destroy
 
       def create
         @task = Task.create(tasks_params)
@@ -15,10 +16,19 @@ module Api
         end
       end
 
+      def destroy
+        @task.archived!
+        success_response(data: @task, model: 'Task', status: :ok)
+      end
+
       private
 
       def load_project
         @project = @user.projects.find(params[:project_id])
+      end
+
+      def load_task
+        @task = @project.tasks.find(params[:id])
       end
 
       def tasks_params
--- app/models/project.rb
+++ app/models/project.rb
@@ -7,4 +7,14 @@ class Project < ApplicationRecord
 
   belongs_to :user
   has_many :tasks
+
+  before_save :archive_tasks
+
+  private
+
+  def archive_tasks
+    return unless status == "archived"
+
+    tasks.each { |task| task.archived! }
+  end
 end
