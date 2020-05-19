94762e691908b743acb6db691edc5ccca419fc12
Andre Oliveira <desmonplays@gmail.com>
2020-05-18 20:17:14 -0400

    (new feature) delete routes of tasks and projects

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :sessions, only: :create
       resources :users, only: %i[show], param: :token
-      resources :projects, only: %i[index create] do
-        resources :tasks, only: :create
+      resources :projects, only: %i[index create destroy] do
+        resources :tasks, only: %i[create destroy]
       end
     end
   end
