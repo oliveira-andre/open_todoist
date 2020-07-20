806327b24c388f3a851329d24495720ee45ded02
Andre Oliveira <desmonplays@gmail.com>
2020-07-20 15:32:13 -0300

    fix: removing delete and create buttons

--- app/javascript/packs/tasksArchiveds/index.vue
+++ app/javascript/packs/tasksArchiveds/index.vue
@@ -9,22 +9,12 @@
                 <li class="has-text-weight-bold" :data-target="project.id" v-on:click.self="toggleTasks">
                   <i class="fas fa-chevron-down is-size-7"></i> {{ project.title }}
                   <span class="has-text-weight-normal is-size-7"> {{ project.tasks.length }}</span>
-                  <div class="is-pulled-right">
-                    <button class="button is-danger is-small" :data-target="project.id" v-on:click.stop.prevent.self="deleteProject">
-                      <i class="fas fa-trash"></i>
-                    </button>
-                    <button class="button is-primary is-small" :data-target="project.id" v-on:click.stop.prevent.self="startNewTask">
-                      <i class="is-pulled-right fas fa-plus"></i>
-                    </button>
-                  </div>
                 </li>
-                <input type="text" :id="'new-task-on-project-'+project.id" class="input is-hidden" placeholder="Type the title of task" v-model="taskTitle" v-on:keyup.enter="createTask">
                 <ul class="tasks" :id="'tasks-from-project-'+project.id">
                   <template v-for="task in project.tasks">
                     <div class="column is-12">
                       <li>
                         {{ task.title }}
-                        <i class="is-pulled-right fas fa-trash has-text-danger" :data-project-id="project.id" :data-target="task.id" v-on:click.self="deleteTask"></i>
                       </li>
                     </div>
                   </template>
@@ -62,5 +52,25 @@
           });
         }
       },
+      methods: {    
+        toggleTasks: function(e) {
+          var icon = e.target.firstElementChild;
+          var tasks = document.querySelector(`#tasks-from-project-${e.target.dataset.target}`);
+
+          if(icon.classList.contains('fa-chevron-down')){
+            icon.classList.remove('fa-chevron-down');
+            icon.classList.add('fa-chevron-right');
+          } else { 
+            icon.classList.remove('fa-chevron-right');
+            icon.classList.add('fa-chevron-down');
+          }
+
+          if(tasks.classList.contains('is-hidden')){
+            tasks.classList.remove('is-hidden');
+          } else {
+            tasks.classList.add('is-hidden');
+          }
+        },
+      }
     }
 </script>
