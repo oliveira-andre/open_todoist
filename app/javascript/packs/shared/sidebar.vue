ac3f3d31fe84402f66a5240ffa4d0f9d9a557f78
Andre Oliveira <desmonplays@gmail.com>
2020-07-21 18:47:40 -0300

    feature: on load dinamicaly check what tab set as current

--- app/javascript/packs/shared/sidebar.vue
+++ app/javascript/packs/shared/sidebar.vue
@@ -2,19 +2,19 @@
   <div id="bSidebar" v-if="is_signed_in">
     <ul class="main_filter is-flex">
       <div class="columns is-multiline">
-        <li v-on:click="switchCurrentTab" class="current">
+        <li v-on:click="switchCurrentTab" :class="{ current: isHome }">
           <router-link v-on:click="switchCurrentTab" to="/">
            <i class="fas fa-inbox has-text-primary is-size-6"></i> 
             Inbox
           </router-link>
         </li>
-        <li v-on:click="switchCurrentTab">
+        <li v-on:click="switchCurrentTab" :class="{ current: isArchiveds }">
           <router-link to="/archiveds">
             <i class="far fa-calendar has-text-info is-size-6"></i>
             Archiveds
           </router-link>
         </li>
-        <li v-on:click="switchCurrentTab">
+        <li v-on:click="switchCurrentTab" :class="{ current: isWeek }">
           <router-link to="/week">
             <i class="far fa-calendar-alt has-text-warning is-size-6"></i>
             Next 7 days
@@ -29,11 +29,16 @@
     export default {
       data: function() {
         return {
-          is_signed_in: false
+          is_signed_in: false,
+          isHome: false,
+          isArchiveds: false,
+          isWeek: false
         }
       },
       created: function() {
-        if(window.location.pathname !== '/login') {
+        let pathName = window.location.pathname;
+
+        if(pathName !== '/login') {
           this.token = localStorage.getItem('token');
           if(this.token == null) {
             this.$router.push('/login');
@@ -49,6 +54,12 @@
               }); 
           }
         }
+
+        if (pathName == '/') { this.isHome = true }
+        else if (pathName == '/archiveds') { this.isArchiveds = true; }
+        else if (pathName == '/week') { this.isWeek = true; }
+
+        console.log({ home: this.isHome, archived: this.isArchiveds, week: this.isWeek })
       },
       methods: {
         switchCurrentTab: function(event) {
