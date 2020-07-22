704e5f2c4ac2491daf25564dac82365c965a5811
Andre Oliveira <desmonplays@gmail.com>
2020-07-21 21:18:28 -0400

    feature: adding skills controller

---
+++ app/controllers/api/v1/skills_controller.rb
@@ -0,0 +1,20 @@
+module Api
+  module V1
+    class SkillsController < ApplicationController
+      before_action :load_user
+      before_action :load_projects
+
+      def index
+        success_response(data: @projects, status: :ok, model: 'Project')
+      end
+
+      private
+
+      def load_projects
+        @projects = @user.projects.active.includes(:tasks)
+                         .where(tasks: { status: 0 }).distinct.limit(5)
+      end
+    end
+  end
+end
+
