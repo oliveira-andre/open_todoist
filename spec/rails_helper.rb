25563fc09e5bc16ee22f6fa7dcec328d4ce4987c
Andre Oliveira <desmonplays@gmail.com>
2020-08-09 02:03:06 -0300

    feat: including factory_bot to rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,6 +20,7 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.include FactoryBot::Syntax::Methods
 end
 
 Shoulda::Matchers.configure do |config|
