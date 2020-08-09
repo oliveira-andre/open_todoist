ff83185ef7c86779d519de40b6781b450590d070
Andre Oliveira <desmonplays@gmail.com>
2020-08-09 03:03:30 -0300

    feat: importing response module of support folder

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,6 +21,7 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.include FactoryBot::Syntax::Methods
+  config.include Response
 end
 
 Shoulda::Matchers.configure do |config|
