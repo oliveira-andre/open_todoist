3ea3000e284feb513aa5b77d46b71acbf82b35a4
Andre Oliveira <desmonplays@gmail.com>
2020-08-09 21:15:12 -0300

    feat: requesting the happy way to create the task, and send invalid params

---
+++ spec/requests/api/v1/tasks_request_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+RSpec.describe 'Task Management', type: :request do
+  let!(:project) { create(:project) }
+
+  context :create do
+    let(:current_path) { "/api/v1/projects/#{project.id}/tasks" }
+
+    context 'invalid params and valid headers' do
+      let(:invalid_params) do
+        {
+          tasks: {
+            title: nil
+          }
+        }
+      end
+      let(:valid_headers) { { 'token' => project.user.authentication_token } }
+
+      before do
+        post current_path, params: invalid_params, headers: valid_headers
+      end
+
+      it { expect(response).to have_http_status(:unprocessable_entity) }
+      it { expect(assigns(:task)).to_not be_valid }
+    end
+
+    context 'valid with params and headers' do
+      let(:valid_params) do
+        {
+          tasks: attributes_for(:task, project: project)
+        }
+      end
+      let(:valid_headers) { { 'token' => project.user.authentication_token } }
+      let(:keys_returned) { %w[id title description status schedule_date] }
+
+      before do
+        post current_path, params: valid_params, headers: valid_headers
+      end
+
+      it { expect(response).to have_http_status(:created) }
+      it do
+        expect(parsed_response.keys).to match_array(keys_returned)
+      end
+    end
+  end
+end
